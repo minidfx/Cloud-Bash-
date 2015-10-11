85850aabcc00f5cda92cf54dd372b50a4dd046de
Burgy Benjamin <benjamin.burgy@master.hes-so.ch>
2015-10-11 13:55:19 +0200

    Prompt password only when the variable OS_PASSWORD isn't defined

--- configuration.sh
+++ configuration.sh
@@ -1,20 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-# file: start_OS_VMs.sh
-# date: 24.09.13
-# description: starts 2 virtual machines on the OpenStack HepiaCloud
+if [ -z "$OS_PASSWORD" ]; then
 
-# add the configuration from Switch
 . "./benjamin.burgy@master.hes-so.ch-openrc.sh"
 
-# set environment for OpenStack command-line tool
-export OS_USERNAME=benjamin.burgy@master.hes-so.ch
-export OS_TENANT_NAME="Tuto OpenStack"
-export OS_AUTH_URL="https://keystone.cloud.switch.ch:5000/v2.0"
-export OS_REGION_NAME=ZH
-
-# set OpenStack VM variables
-export IMAGE=SNAPSHOT_ID
-export FLAVOR=1
-export KEY=student-key
-
+fi
