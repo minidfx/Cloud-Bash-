e10b46135c7a20a3fc7225665dd5b4033e2f704b
Burgy Benjamin <minidfx@gmail.com>
2015-10-12 14:16:08 +0200

    Improvement about the post-install script, floatting ip and fixed instance name.

--- delivrable.sh
+++ delivrable.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-SERVER1="MongoDB"
-SERVER2="RESTClient"
-SERVER3="RESTServer"
-SERVERS="$SERVER1,$SERVER2,$SERVER3"
-
-./restoreSnapshots.sh $SERVERS
-
-SERVER1_IP=`./getIP.sh $SERVER1`
-
-./executeCommand.sh $SERVER2 'python ~/Downloads/pyClient.py $SERVER1_IP &'
-./executeCommand.sh $SERVER3
+./restoreSnapshots.sh
 
 echo ""
 echo "Enter A to abort the script and deleting the VMs previously created."
@@ -22,9 +12,4 @@ do
        read -a INPUT
 done
 
-IFS=',' read -a snapshots <<< "$1"
-
-for ID in $snapshots
-do
-	source deleteVM.sh $ID
-done
+./deleteVM.sh MongoDB RESTServer RESTClient
--- restoreSnapshots.sh
+++ restoreSnapshots.sh
@@ -2,22 +2,40 @@
 
 . "./configuration.sh"
 
-if [ -z "$1" ]; then
-	echo "ERROR: You have to specify a list of Snapshot IDs separate by a comma."
-	echo "Usage: $0 <id1,id2,idn>"
-	exit 1
-fi
-
-IFS=',' read -a snapshots <<< "$1"
 FLAVOR=baaba9ad-9ee5-4f1d-a7a1-dd4b55c18b63
 NETWORK_ID=685a78ae-602e-4b80-9b54-97bdfcef5c2f
+SECURITY_GROUP=anywhere
 
-for ID in $snapshots
-do
-	echo -n "Restoring and starting $ID ... "
-	nova boot --flavor=$FLAVOR --image=$ID --nic net-id=$NETWORK_ID $ID > /dev/null
-	echo "Done".
-done
+echo -n "Restoring and starting MongoDB ... "
+nova boot --flavor=$FLAVOR --image=MongoDB --nic net-id=$NETWORK_ID --security-groups $SECURITY_GROUP MongoDB > /dev/null
+nova floating-ip-associate MongoDB 86.119.33.239
+echo "Done".
+
+./cacheServerList.sh
+
+MongoDB_IP=`./getIP.sh MongoDB`
+
+# Create the startup script the RESTClient
+echo -e "#\!/bin/bash\n\n\pyhton restclient.py $MongoDB_IP &" > restClient.sh
+# Create the startup script for the RESTServer
+echo -e "#\!/bin/bash\n\n\pyhton restserver.py $MongoDB_IP &" > restServer.sh
+
+echo -n "Restoring and starting RESTServer ... "
+nova boot --user-data=./restServer.sh --flavor=$FLAVOR --image=RESTServer --nic net-id=$NETWORK_ID --security-groups $SECURITY_GROUP RESTServer > /dev/null
+nova floating-ip-associate RESTServer 86.119.33.32
+echo "Done".
+
+echo -n "Restoring and starting RESTClient ... "
+nova boot --user-data=./restClient.sh --flavor=$FLAVOR --image=RESTClient --nic net-id=$NETWORK_ID --security-groups $SECURITY_GROUP RESTClient > /dev/null
+nova floating-ip-associate RESTClient 86.119.33.34
+echo "Done".
+
+rm restClient.sh
+rm restServer.sh
+
+echo -n "Waiting 15 seconds for servers are started ... "
+sleep 15
+echo "Done."
 
 ./cacheServerList.sh
 
@@ -26,19 +44,15 @@ if [ ! -f "/tmp/servers.list" ]; then
 			exit 1
 fi
 
-echo -n "Waiting 7 seconds ... "
-sleep 7
-echo "Done."
-
-for ID in $snapshots
+for arg in "MongoDB" "RESTServer" "RESTClient"
 do
-	network=`cat /tmp/servers.list | grep $ID | sed -n 's/.*\=\([^|]*\).*/\1/p'`
+	network=`cat /tmp/servers.list | grep $arg | sed -n 's/.*\=\([^|]*\).*/\1/p'`
 	IFS=', ' read -ra addresses <<< "$network"
 
 	if [ ${#addresses[@]} -lt 1 ]; then
-		echo "ERROR: The server $ID isn't started!"
+		echo "ERROR: The server $arg isn't started!"
 	else
-		echo -n "Checking whether the server $ID (${addresses[0]}) is running ... "
+		echo -n "Checking whether the server $arg (${addresses[0]}) is running ... "
 
 		# I send an empty string because the nc block when no data is sent.
 		result=`echo "" | nc ${addresses[0]} 22 | grep --color=never SSH`
